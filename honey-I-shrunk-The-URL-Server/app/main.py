8bc2eced1784ae2549f2548f218dbd6117ddb4eb
CrwnT1 <82129548+CrwnT1@users.noreply.github.com>
2024-01-06 19:54:38 -0800

    Add files via upload

---
+++ honey-I-shrunk-The-URL-Server/app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, render_template
+import firebase_admin
+from firebase_admin import db
+import os
+
+cred_obj = firebase_admin.credentials.Certificate('./ServiceAccountKey.json')
+default_app = firebase_admin.initialize_app(cred_obj,  {
+	'databaseURL': 'https://honey-i-shrunk-the-url-default-rtdb.firebaseio.com/'
+	})
+
+app = Flask(__name__, static_folder='./build/static', template_folder="./build" )
+
+@app.route("/")
+def hello_world():
+    return redirect("/app")
+
+@app.route("/app")
+def homepage():
+    return render_template('index.html')
+
+@app.route('/<path:generatedKey>', methods=['GET'])
+def fetch_from_firebase(generatedKey):
+    ref = db.reference("/"+ generatedKey)
+    data = ref.get()
+    if not data:
+        return '404 not found'
+    else:
+        longURL = data['longURL']
+        return redirect(longURL)
